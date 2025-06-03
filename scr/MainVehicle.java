d31c4bf5b4fc48c9ba9d3d8e0e649c7db4372747
Aminkhishig <aminkhishiga@gmail.com>
2025-06-03 11:44:47 +0800

    Add initial Vehicle inheritance example code

---
+++ scr/MainVehicle.java
@@ -0,0 +1,111 @@
+package day2.scr;
+
+class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println("Vehicle Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "  " +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year;
+                }
+}
+
+class Car extends Vehicle {
+
+    private double trunkSize;
+
+    public Car(String make, String model, int year, double trunkSize) {
+        super(make, model, year);
+        this.trunkSize = trunkSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Trunk Size: " + trunkSize + " cubic feet");
+    }
+
+    public double getTrunkSize() {
+        return trunkSize;
+    }
+
+    public void setTrunkSize(double trunkSize) {
+        if (trunkSize > 0) {
+            this.trunkSize = trunkSize;
+        } else {
+            System.out.println("Trunk size must be positive.");
+        }
+    }
+}
+
+class Truck extends Vehicle {
+
+    private double payloadCapacity;
+
+    public Truck(String make, String model, int year, double payloadCapacity) {
+        super(make, model, year);
+        this.payloadCapacity = payloadCapacity;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Payload Capacity: " + payloadCapacity + " tons");
+    }
+
+    public double getPayloadCapacity() {
+        return payloadCapacity;
+    }
+
+    public void setPayloadCapacity(double payloadCapacity) {
+        if (payloadCapacity > 0) {
+            this.payloadCapacity = payloadCapacity;
+        } else {
+            System.out.println("Payload capacity must be positive.");
+        }
+    }
+}
+
+public class MainVehicle {
+    public static void main(String[] args) {
+
+        Car car = new Car("Honda", "Civic", 2021, 15.0);
+        System.out.println(car);
+        car.displayDetails();
+
+        System.out.println();
+        
+        Truck truck = new Truck("Ford", "F-150", 2019, 2.5);
+        System.out.println(truck);
+        truck.displayDetails();
+    }
+}
